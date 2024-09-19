70a7f31605de08f026468f4b2c703415ab241281
Yogesh <yogesh@ix.cumberlandlabs.io>
2024-09-19 23:10:33 +0530

    server sign url

--- src/turnkey/config/turnKeyConfig.ts
+++ src/turnkey/config/turnKeyConfig.ts
@@ -2,6 +2,6 @@ export const turnKeyConfig = {
     apiBaseUrl: 'https://api.turnkey.com',
     defaultOrganizationId: '1a549f1d-e8b2-45a4-81e3-2cd7a8c5b746',
     rpId: 'turnkey-sigma.vercel.app',
-    // serverSignUrl: 'https://883d-2405-201-680b-a16a-916c-dc1b-5ee6-8632.ngrok-free.app/',
+    serverSignUrl: 'https://turnkey-sigma.vercel.app/',
     iframeUrl: 'https://auth.turnkey.com', // not necessary for this example
 };
