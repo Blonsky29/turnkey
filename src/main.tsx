56f0b336c86037b343658dd58d7c74338a47f5ed
Yogesh <yogesh@ix.cumberlandlabs.io>
2024-09-19 23:59:14 +0530

    sdk added

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import './index.css'
 import App from './App'
 import eruda from 'eruda'
 import TurnKeyProviderConfig from './turnkey'
+import WebApp from '@twa-dev/sdk'
 
 eruda.init()
+WebApp.ready()
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
