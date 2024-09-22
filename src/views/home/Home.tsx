5176d26fc3f0fb4547df3f8b9aeab498080e49a7
Yogesh <yogesh@ix.cumberlandlabs.io>
2024-09-22 12:46:27 +0530

    route to register key

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div className=' text-white'>Home</div>
+    <div className=' text-white'>
+      <Link to={'/register-passkey'}>
+      To Register Pass Key
+      </Link>
+    </div>
   )
 }
